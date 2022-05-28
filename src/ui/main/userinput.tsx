848ef4f83395ac91bdeaf25f5c41fa19207afe27
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2022-05-28 17:34:20 +0930

    changed select component from antd to react bootstrap

--- src/ui/main/userinput.tsx
+++ src/ui/main/userinput.tsx
@@ -1,4 +1,4 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Row, Form } from "react-bootstrap";
 import { Input, Radio, Select } from "antd";
 import {
   updateAge,
@@ -44,110 +44,116 @@ const UserInput = () => {
     <div className="paddingBottom">
       <img src="/logo.png" alt="logo Fitness Playground" height="250rem" className="my-3" />
       <div style={{ marginTop: "2em" }}>
-        <Col className="mx-auto text-left">
-          <Row>
-            <Col xs="6" md="8">
-              My weight is:
-            </Col>
-            <Col xs="4" className="my-1">
-              <Input
-                suffix="KG"
-                type="number"
-                value={weight}
-                className="inputCustom"
-                onChange={(e) => setWeight(e.target.value)}
-                style={{ width: 100 }}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6" md="8">
-              My height is:
-            </Col>
-            <Col xs="4" className="my-1">
-              <Input
-                suffix="CM"
-                type="number"
-                value={height}
-                className="inputCustom"
-                onChange={(e) => setHeight(e.target.value)}
-                style={{ width: 100 }}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6" md="8">
-              My age is:
-            </Col>
-            <Col xs="4" className="my-1">
-              <Input
-                suffix="years"
-                type="number"
-                value={age}
-                className="inputCustom"
-                onChange={(e) => setAge(e.target.value)}
-                style={{ width: 100 }}
-              />
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6" md="8">
-              I'm :
-            </Col>
-            <Col xs="4" className="my-1">
-              <Radio.Group name={"sex"} onChange={(e) => dispatch(updateSex(e.target.value))} value={sex}>
-                <Radio value={true}>Male</Radio>
-                <Radio value={false}>Female</Radio>
-              </Radio.Group>
-            </Col>
-          </Row>{" "}
-          <Row>
-            <Col xs="6" md="8">
-              <strong>I want to:</strong>
-            </Col>
-            <Col xs="4" className="my-1">
-              <Select
-                defaultValue={goal}
-
-                style={{
-                  background: "rgba(116, 105, 247, 0.15)",
-                  width: 140,
-                  borderRadius: "30px",
-                }}
-                onChange={(e) => dispatch(updateGoal(e))}>
-                <Option value={"cut"}>Lose weight</Option>
-                <Option value={"maintain"}>Maintain</Option>
-                <Option value={"gain"}>Gain muscle</Option>
-              </Select>
-            </Col>
-          </Row>
-          <Row>
-            <Col xs="6" md="8">
-              <strong>I want my meal plan:</strong>
-            </Col>
-            <Col xs="4" className="my-1">
-              <Select
-                defaultValue={mealplan}
-                style={{
-                  background: "rgba(116, 105, 247, 0.15)",
-                  width: 140,
-                  borderRadius: "30px",
-                }}
-                onChange={(e) => dispatch(updateMealPlan(e))}>
-                <Option value={"standard"}>Standard</Option>
-                <Option value={"custom"}>Customized</Option>
-              </Select>
-            </Col>
-          </Row>{" "}
-          <Row>
-            <Col xs="6" md="8"></Col>
-            <Col xs="3" className="my-1">
-              <Link href="/diets" passHref>
-                <Button className="purpleButton float-right">GO!</Button>
-              </Link>
-            </Col>
-          </Row>
-        </Col>
+        <Form>
+          <Col className="mx-auto text-left">
+            <Row>
+              <Col xs="6" md="8">
+                My weight is:
+              </Col>
+              <Col xs="4" className="my-1">
+                <Input
+                  suffix="KG"
+                  type="number"
+                  value={weight}
+                  className="inputCustom"
+                  onChange={(e) => setWeight(e.target.value)}
+                  style={{ width: 100 }}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6" md="8">
+                My height is:
+              </Col>
+              <Col xs="4" className="my-1">
+                <Input
+                  suffix="CM"
+                  type="number"
+                  value={height}
+                  className="inputCustom"
+                  onChange={(e) => setHeight(e.target.value)}
+                  style={{ width: 100 }}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6" md="8">
+                My age is:
+              </Col>
+              <Col xs="4" className="my-1">
+                <Input
+                  suffix="years"
+                  type="number"
+                  value={age}
+                  className="inputCustom"
+                  onChange={(e) => setAge(e.target.value)}
+                  style={{ width: 100 }}
+                />
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6" md="8">
+                I'm :
+              </Col>
+              <Col xs="4" className="my-1">
+                <Radio.Group name={"sex"} onChange={(e) => dispatch(updateSex(e.target.value))} value={sex}>
+                  <Radio value={true}>Male</Radio>
+                  <Radio value={false}>Female</Radio>
+                </Radio.Group>
+              </Col>
+            </Row>{" "}
+            <Row>
+              <Col xs="6" md="8">
+                <strong>I want to:</strong>
+              </Col>
+              <Col xs="4" className="my-1">
+                <Form.Control
+                  as="select"
+                  defaultValue={goal}
+                  style={{
+                    background: "rgba(116, 105, 247, 0.15)",
+                    width: 140,
+                    borderRadius: "30px",
+                  }}
+                  onChange={(e) => {
+                    console.log(e.target.value)
+                    dispatch(updateGoal(e.target.value));
+                  }}>
+                  <option value={"cut"}>Lose weight</option>
+                  <option value={"maintain"}>Maintain</option>
+                  <option value={"gain"}>Gain muscle</option>
+                </Form.Control>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs="6" md="8">
+                <strong>I want my meal plan:</strong>
+              </Col>
+              <Col xs="4" className="my-1">
+                <Form.Control
+                  as="select"
+                  defaultValue={mealplan}
+                  style={{
+                    background: "rgba(116, 105, 247, 0.15)",
+                    width: 140,
+                    borderRadius: "30px",
+                  }}
+                  onChange={(e) => dispatch(updateMealPlan(e.target.value))}>
+                  <option value={"standard"}>Standard</option>
+                  <option value={"custom"}>Customized</option>
+                </Form.Control>
+              </Col>
+            </Row>{" "}
+            <Row>
+              <Col xs="6" md="8"></Col>
+              <Col xs="3" className="my-1">
+                <Link href="/diets" passHref>
+                  <Button className="purpleButton float-right">GO!</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Col>
+        </Form>
       </div>
       {/*<img src={galleryDot} alt="" className="gallery-home-two__dots" />*/}
     </div>
