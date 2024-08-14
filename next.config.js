0a79d85310bb73187a36c39ab100c75c2a7199e8
Frank Espinoza <frank.espinoza@ucsp.edu.pe>
2024-08-14 11:41:46 +0930

    Rebuilt package-lock.json for node v20, cleaned up unnecessary packages on package.json

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 const withPlugins = require("next-compose-plugins");
 
-// prettier-ignore
-module.exports = withPlugins(
-    [],
-  {
-    webpack: (config) => {
-      config.module.rules.push({
-        test: /\.csv$/i,
-        use: "raw-loader",
-      });
-      return config;
-    },
-  }
-);
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.csv$/i,
+      use: "raw-loader",
+    });
+    return config;
+  },
+};
+
+module.exports = withPlugins([], nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,26 @@
 import store from "lib/redux/store";
-import "ui/styles/fonts.css"
+import "ui/styles/fonts.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "ui/styles/antd.css";
 import "ui/styles/app.scss";
 import "ui/styles/custom.scss";
 import { AppProps } from "next/app";
 import React, { useEffect } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { Hydrate } from "react-query/hydration";
 import { Provider } from "react-redux";
 import { Navbar } from "ui/main/navbar";
 import TagManager from "react-gtm-module";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const queryClient = new QueryClient();
-
   useEffect(() => {
     TagManager.initialize({ gtmId: "GTM-M426K9P" });
   }, []);
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Provider store={store}>
-          <Navbar>
-            <Component {...pageProps} />
-          </Navbar>
-        </Provider>
-      </Hydrate>
-    </QueryClientProvider>
+    <Provider store={store}>
+      <Navbar>
+        <Component {...pageProps} />
+      </Navbar>
+    </Provider>
   );
 }
 
